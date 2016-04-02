5bb38445fe382cd3020a7489fbd0cbcedb4d17bc
bumshakabum <bumcrystlbum@gmail.com>
2016-04-02 03:07:38 -0600

    imported twitter api in server.js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
